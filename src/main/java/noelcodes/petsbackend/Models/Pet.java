817829b0122f6faafa62333b2ab94f648423d74b
Noel Covarrubias <no3lcodes@gmail.com>
2023-01-06 10:16:27 -0800

    Set pet fetch type to lazy

--- src/main/java/noelcodes/petsbackend/Models/Pet.java
+++ src/main/java/noelcodes/petsbackend/Models/Pet.java
@@ -25,7 +25,7 @@ public class Pet {
     @Column(nullable = false)
     private String furColor;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id", nullable = false)
     @JsonBackReference
     private PetOwner owner;
